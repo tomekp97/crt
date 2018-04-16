b432c252615f9b015a86061cb290afd25a78cc76
Tomek <tomek@evolvedigital.co.uk>
2018-04-17 00:57:04 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+var pastInput = document.getElementById('past-user-input');
+
+// User Input
+var textInput = document.getElementsByClassName('user-text')[0];
+var _textInputExisting = textInput.textContent;
+var _textInputFinal = '';
+
+// Prompt
+
+
+function Prompt(user, atWhat, caret)
+{
+    var promptValue = user + '@' + atWhat + ' ' + caret + ' ';
+
+    var prompt = document.createElement('span');
+    prompt.className = 'prompt';
+    prompt.innerText = promptValue;
+
+    return prompt;
+}
+
+function GetUserInput(terminalPrompt)
+{
+    console.log("Listening for keypress...");
+
+    textInput.prepend(terminalPrompt);
+
+    document.onkeypress = function(e)
+    {
+        e = e.keyCode;
+        var char = String.fromCharCode(e);
+        console.log(e + ' - ' + char);
+
+        textInput.textContent = _textInputExisting + char;
+        _textInputExisting = textInput.textContent;
+
+        // If keypress is Enter
+        if (e === 13) {
+            _textInputFinal = _textInputExisting;
+
+            textInput.textContent = '';
+            _textInputExisting = '';
+
+            console.log("User input: " + _textInputFinal);
+
+            DisplayPastInput(_textInputFinal, terminalPrompt);
+
+            return _textInputFinal;
+        }
+    }
+}
+
+function DisplayPastInput(toDisplay, terminalPrompt)
+{
+    console.log(terminalPrompt);
+    var pastInputLine = document.createElement('span');
+    pastInputLine.className = 'past-line';
+    pastInputLine.innerText = terminalPrompt.innerText + toDisplay;
+
+    pastInput.appendChild(pastInputLine);
+}
+
+window.onload = function() {
+    var prompt = Prompt("tomek","matrix","$");
+    GetUserInput(prompt);
+}
